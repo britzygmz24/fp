5b0a8ec412226b8ac622b26a35a9249ba53f836c
britzygmz24 <britzygomezflores8727770@gmail.com>
2021-09-17 13:51:20 -0500

    mi examen phyton

---
+++ examen.py
@@ -0,0 +1,23 @@
+Datos del alumno
+Nombre del alumno:
+Britzy Michel Gomez
+Lugar de residencia:
+Rincon de Romos, Aguascalientes
+Fecha de nacimiento:
+02 de octubre de 2003
+Color favorito:
+Negro
+Animal favorito:
+Tigre
+¿Qué es la programación?
+Esta asociada a la informatica y se utiliza para ordenar las acciones necesarias para realizar un proyecto
+¿Por qué es importante la programación?
+Es importante para el avance tecnologico en las industrias necesitan sitios, aplicaciones y codigos
+Explique con sus palabras que es herencia
+Son las caracteristicas que un objeto hereda de una clase de la que es proveniente
+Explique con sus palabras que es poliformismo.
+Un objeto puede tener el mismo nombre en diversas clases y funcionar distinto
+Explique con sus palabras que es abstracción.
+Es quitar las propiedades y acciones de un objeto para dejar solo las necesarias
+Explique con sus palabras que es encapsulamiento.
+Cuando un objeto oculta su o sus funcionalidades internas como la television
