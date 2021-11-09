f4241b3a1ecd34743f60d26cc448a0083ae56eb7
britzygmz24 <britzygomezflores8727770@gmail.com>
2021-11-09 10:10:59 -0600

    diccionario

---
+++ UNIDAD 3/diccionario.py
@@ -0,0 +1,34 @@
+automovil = {
+	"modelo" : "2020",
+	"marca" : "nissan",
+	"color" :  "rojo",
+}
+
+television =  {
+	"tamaño" : "50 pulgadas",
+	"resolucion" : "4096 x 2160 píxeles",
+	"entradas" : "usb hdmi",
+}
+
+telefono = {
+	"modelo" : "2021",
+	"funda" : "rosa",
+	"marca" : "samsung",
+	"color" : "negro",
+}
+
+libro = {
+	"hojas" : "escritos",
+	"ejercicios" : "problemas",
+	"lecturas" : "leer",
+
+}
+
+ventana = {
+	"color" : "blanco",
+	"diseño" : "formas",
+	"vidrios" : "proteccion",
+}
+
+print(automovil, television, telefono, libro, ventana)
+	
