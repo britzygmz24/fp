002721dd158f276924ede206a9f59da7880b07d7
britzygmz24 <britzygomezflores8727770@gmail.com>
2021-11-29 14:19:22 -0600

    programas

---
+++ UNIDAD 4/programa sueldo del trabajador.py
@@ -0,0 +1,11 @@
+trabajador1 ={
+	"sueldo por hora" = "$150"
+	"dia1 2 horas" = "$300"
+	"dia2 4 horas" = "$600"
+	"dia3 3 horas y ½" = "$575"
+
+ESCRIBIR (Dame la suma de dia1 2 horas, dia2 4 horas, dia3 3 horas y ½ ),
+
+print (dia1 2 horas + dia2 4 horas + dia3 3 horas y ½)
+
+else (cuantas horas trabajo)
---
+++ UNIDAD 4/programa2.py
@@ -0,0 +1,28 @@
+programaquepreguntacantidades
+usuario ={
+	"cantidad capital" = "$1000000"
+	"interes anual" = "18.2%"
+	"iva" = "16%"
+	"numero de años" = "10 años"
+	"crecimiento de capital por años" = "12%"
+
+} 
+
+
+
+	CARACTER
+INICIO
+	ESCRIBIR (cantidad capital);
+	LEER (cantidad capital);
+	DAME (cantidad capital);
+	ESCRIBIR (interes anual);
+	DAME (interes anual);
+	ESCRIBIR (iva);
+	DAME (iva);
+	ESCRIBIR (numero de años);
+	DAME (numero de años);
+	ESCRIBIR (crecimiento de capital por años);
+	DAME (crecimiento de capital por años);
+
+	print(1000000 x 18.2% x 16% x 10 x 12%)
+FIN
