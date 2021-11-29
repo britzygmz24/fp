6a3550c7d055c0ece0b7611fcbddbfe7693023b6
britzygmz24 <britzygomezflores8727770@gmail.com>
2021-11-29 13:28:35 -0600

    unidad4

---
+++ UNIDAD 4/numeros.py
@@ -0,0 +1,21 @@
+n=int(input(´dame un numero´))
+def fibonacci_iter(n):
+	a= 2
+	b=4
+	if n == 2:
+		print(´0´)
+
+
+	elif n== 6:
+		print(´0´,´2´)
+	else:
+		print(´0´)
+		print(a)
+		print(b)
+		for i in range(n-3):
+			total = a+b
+			b=range
+			a=total
+			print(total)
+
+fibonacci_iter(n)
